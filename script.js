061d873aead4514f427d7deae93a5c0ccd0f837e
Luis Alburez <alburezluis@gmail.com>
2025-03-22 10:44:39 -0600

    commit para subir los archivos al repositorio

---
+++ script.js
@@ -0,0 +1,16 @@
+/* Ahora que sabes cÃ³mo configurar y usar un bloque for...of, practiquemos
+con un desafÃ­o de cÃ³digo escribiendo algo de cÃ³digo.
+
+Primero, escribamos un programa que nos ayude a mejorar nuestras habilidades
+ortogrÃ¡ficas.
+
+Dado spellingWord, utiliza un bucle for...of para registrar cada letra
+usando console.log().*/
+
+const spellingWord = "hipopótamo";
+
+//Escribe aquÃ­ tu cÃ³digo
+
+for (const letra of spellingWord) {
+  console.log(letra);
+}
